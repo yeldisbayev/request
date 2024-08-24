fb743f5206272dd3897f538cce078ec1847df043
Duisen Yeldisbayev <duisen.yeldisbayev.yeldisbayev@tele2.kz>
2024-08-25 01:38:46 +0500

    feat(response): comment for Success method

--- response.go
+++ response.go
@@ -8,6 +8,7 @@ type Response struct {
 	*http.Response
 }
 
+// Success checks response status code for success.
 func (r *Response) Success() bool {
 	return r.StatusCode >= 200 && r.StatusCode < 300
 }
