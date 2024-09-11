aca0b9f57cc6742b2d0ca6a77e515af00a5f2d7f
Duisen Yeldisbayev <duisen.yeldisbayev.yeldisbayev@tele2.kz>
2024-09-12 02:13:17 +0500

    feat(client): create RoundTripper

---
+++ round_tripper.go
@@ -0,0 +1,9 @@
+package req
+
+import "net/http"
+
+type RoundTripper func(*http.Request) (*http.Response, error)
+
+func (rt RoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	return rt(req)
+}
